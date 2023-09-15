d60d0afb2c6f7e2f95cf361e80efdc6a97e1400f
Santiago <blookrunch@gmail.com>
2023-09-15 12:47:57 -0600

    agregados DAO y controller

--- AluraHotel-Esp-Base/src/views/RegistroHuesped.java
+++ AluraHotel-Esp-Base/src/views/RegistroHuesped.java
@@ -60,6 +60,9 @@ public class RegistroHuesped extends JFrame {
 	 */
 	public RegistroHuesped() {
 		
+	}
+	public RegistroHuesped(int idReserva) {
+		
 		setIconImage(Toolkit.getDefaultToolkit().getImage(RegistroHuesped.class.getResource("/imagenes/lOGO-50PX.png")));
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setBounds(100, 100, 910, 634);
@@ -210,6 +213,9 @@ public class RegistroHuesped extends JFrame {
 		txtNreserva.setColumns(10);
 		txtNreserva.setBackground(Color.WHITE);
 		txtNreserva.setBorder(javax.swing.BorderFactory.createEmptyBorder());
+		txtNreserva.setEditable(false);
+		String id = String.valueOf(idReserva);
+		txtNreserva.setText(id);
 		contentPane.add(txtNreserva);
 		
 		JSeparator separator_1_2 = new JSeparator();
